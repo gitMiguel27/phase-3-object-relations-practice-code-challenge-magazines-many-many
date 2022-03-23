9a037e8c6dc89c86fd7655111cf3aa543a8aedff
Miguel Nazario <mnazario35@gmail.com>
2022-03-23 17:31:34 -0400

    commit tree d1b0e1aadabbb7ba4292b9c030a42090c0457eeb

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,15 @@
 class Magazine < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :readers, through: :subscriptions
+
+    def email_list
+        emails = self.readers.map {|reader| reader.email}
+        emails.join(";")
+    end
+
+    def self.most_popular
+        subscriptions_array = self.all.map {|magazine| magazine.subscriptions}
+        most_subs = subscriptions_array.max_by {|subs| subs.length}
+        self.all.find_by subscriptions: most_subs
+    end
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,19 @@
 class Reader < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+
+    def subscribe(magazine, price)
+        Subscription.create(price: price, magazine_id: magazine.id, reader_id: self.id)
+    end
+
+    def total_subcription_price
+        all_prices = self.subscriptions.map {|subscription| subscription.price}
+        all_prices.sum()
+    end
+
+    def cancel_subscription(magazine)
+        self.subscriptions.destroy_by(magazine_id: magazine.id)
+        # subscription_to_cancel = self.subscriptions.find_by magazine_id: magazine.id
+        # subscription_to_cancel.destroy
+    end
 end
